6524450c1633b0fe5977eb3ffb9b6c4d2816b87b
LucaWu114514 <jonsenyao@163.com>
2025-02-22 20:05:02 +0800

    Update 外星人入侵.py

--- py_work/外星人入侵.py
+++ py_work/外星人入侵.py
@@ -1,47 +1,53 @@
 from random import randint
-class Alieninvasion:
-    input('press enter to start game!')
-    kill_number = 0
-    while True:
-        if kill_number <= 1:
-                    alien = 'alien'
-        elif kill_number >= 1:
-                    alien = 'aliens'
-        randint_number = randint(1,5)
-        anser = input('Do you want to kill aliens?')
-        yes = 'yes'
-        no = 'no'
-        if anser == yes:
-            if randint_number == 1 or randint_number == 2 or randint_number == 3 or randint_number == 4:
-                print('You kill an alien')
-                kill_number += 1
-            elif randint_number == 5:
-                input('You kill no alien')
-                input('Now it fights with you!')
-                input('Your gun was break!')
-                new_anser = input('run or fight?')
-                if new_anser == 'run':
-                    input('You run away,you have a new gun now!')
-                    continue
-                elif new_anser == 'fight':
-                    dead = randint(1,2)
-                    if dead == 1:
-                        print('You win,you have anew gun now!')
-                        kill_number += 1
-                        continue
-                    elif dead == 2:
-                        input('You dead!')
-                        input(f'You kill {kill_number} {alien}')
-                        break
-                else:
-                      print('?')
-                      print('Game over')
-                      break
-        elif anser == no:
-                print('Oh!What a pity!')
-                print(f'You kill {kill_number} {alien}')
-                break
+
+class AlienInvasion:
+    def __init__(self):
+        self.kill_number = 0
+
+    def start_game(self):
+        input('Press enter to start game!')
+        while True:
+            self.play_round()
+
+    def play_round(self):
+        alien = 'alien' if self.kill_number <= 1 else 'aliens'
+        randint_number = randint(1, 5)
+        answer = input('Do you want to kill aliens? (yes/no): ').strip().lower()
+        
+        if answer == 'yes':
+            self.process_kill_attempt(randint_number, alien)
+        elif answer == 'no':
+            print(f'Oh! What a pity! You killed {self.kill_number} {alien}')
+            print('Game over')
+            return
         else:
             print('?')
             print('Game over')
-            break    
+            return
+
+    def process_kill_attempt(self, randint_number, alien):
+        if randint_number in [1, 2, 3, 4]:
+            print('You killed an alien!')
+            self.kill_number += 1
+        elif randint_number == 5:
+            print('You killed no alien. Now it fights with you! Your gun was broken!')
+            self.process_fight_or_run()
+
+        print(f'You have killed {self.kill_number} {alien}')
+
+    def process_fight_or_run(self):
+        new_answer = input('Run or fight? (run/fight): ').strip().lower()
+        if new_answer == 'run':
+            print('You ran away, you have a new gun now!')
+        elif new_answer == 'fight':
+            dead = randint(1, 2)
+            if dead == 1:
+                print('You win, you have a new gun now!')
+                self.kill_number += 1
+            else:
+                print('You died!')
+                print('Game over')
+
+if __name__ == "__main__":
+    game = AlienInvasion()
+    game.start_game()
